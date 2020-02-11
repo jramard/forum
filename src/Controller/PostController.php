40782d8cc444b58e0a0cfba2eee40f5f96db965d
Julien Ramard <julien.ramard@gmail.com>
2020-02-11 23:49:05 +0100

    feat: added posts vue

---
+++ src/Controller/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/post", name="post")
+     */
+    public function index(PostRepository $postRepository)
+    {
+        $posts = $postRepository->findAll();
+
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+}
